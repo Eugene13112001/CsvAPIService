c47cd3d69bee2d713d8034bb2e8152a7896e8db7
Евгений Степанов <m1900795@edu.misis.ru>
2023-03-24 08:53:19 +0300

    #444

---
+++ CsvAPIService/Containers/CSVService.cs
@@ -0,0 +1,53 @@
+﻿using CsvHelper;
+using System.Collections;
+using System.Globalization;
+using System.IO;
+using CsvAPIService.Models;
+using static System.Net.Mime.MediaTypeNames;
+using DnsClient.Protocol;
+
+namespace CsvAPIService.Containers
+{
+    public interface ICSVServiceReader
+    {
+        public Task<CsvModel> ReadCSV (Stream file);
+    }
+    public class CSVServiceReader : ICSVServiceReader
+    {
+        public async Task<CsvModel>  ReadCSV (Stream file)
+        {
+            int count = 0;
+            using (StreamReader reader = new StreamReader(file))
+            {
+                string? line;
+                while ((line = await reader.ReadLineAsync()) != null)
+                {
+
+                    string[] words = line.Split(new char[] { ',' });
+
+                    count += 1;
+                }
+            }
+            return records;
+        }
+    }
+
+    public interface ICSVServiceWriter
+    {
+        public Task<string> WriteCSV(string text, string path);
+    }
+    public class CSVServiceWriter : ICSVServiceWriter
+    {
+        public async Task<string> WriteCSV(string text , string path)
+        {
+
+            // полная перезапись файла 
+            using (StreamWriter writer = new StreamWriter(path, false))
+            {
+                await writer.WriteLineAsync(text);
+            }
+            
+            return path;
+        }
+    }
+}
---
+++ CsvAPIService/Models/CsvModel.cs
@@ -0,0 +1,20 @@
+﻿using MongoDB.Driver;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace CsvAPIService.Models
+{
+    public class CsvModel
+    {
+        [BsonId]
+        public ObjectId Id { get; set; }
+
+        public string Path { get; set; }
+        [BsonExtraElements]
+        public Dictionary<string, int> Count { get; set; }
+        [BsonExtraElements]
+        public Dictionary<string, object>? Values { get; set; }
+
+
+    }
+}
